5e4fb995221cc452fa982e502ce6abfeb678842e
Nattaphol <rootncdev2015@gmail.com>
2018-12-20 17:01:10 +0700

    Move machinesort to main

--- my_lib/main.pl
+++ my_lib/main.pl
@@ -26,6 +26,22 @@ find_greater(machine(N,Kw1,M),machine(_,Kw2,_),machine(N,Kw1,M)) :-
 find_greater(machine(_,Kw1,_),machine(N,Kw2,M),machine(N,Kw2,M)) :-
   Kw1 < Kw2.
 
+is_less_than(machine(_, Kw1, _), machine(N,Kw2,M)):-
+    Kw1 < Kw2.
+
+pivoting(H,[],[],[]).
+pivoting(H,[X|T],[X|L],G):- 
+    is_less_than(X, H),
+    pivoting(H,T,L,G).
+pivoting(H,[X|T],L,[X|G]):- 
+    pivoting(H,T,L,G).
+
+machine_sort(List,Sorted):-q_sort(List,[],Sorted).
+q_sort([],Acc,Acc).
+q_sort([H|T],Acc,Sorted):-
+	pivoting(H,T,L1,L2),
+	q_sort(L1,Acc,Sorted1),q_sort(L2,[H|Sorted1],Sorted).
+
 % split a list into 3 lists on conditions
 % split([], [], [], []).
 % split([H|T], [H|L1], L2, L3) :- 
