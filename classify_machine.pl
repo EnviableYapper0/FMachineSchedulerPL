124c37eb8e5e789ae57e573496fcfd107fa5cfb3
Ubuntu <laizen@PrologDev.3aqmce3q3vvele5dtzq5vx12ud.ix.internal.cloudapp.net>
2018-12-22 06:55:22 +0000

    Allow machine split

--- classify_machine.pl
+++ classify_machine.pl
@@ -3,7 +3,7 @@ machine_time_less_than(machine(_, X, _), Y):-
 
 get_time(machine(_,X,_), X).
 
-classify_not_peak([H|T], Y, A, A1):-
+classify_not_peak([H|T], Y, A, A1, B):-
     get_time(H, X),
     print(a),
     print(X),
@@ -15,10 +15,12 @@ classify_not_peak([H|T], Y, A, A1):-
     Y1 is Y - X,
     append(A1, [H], Z),
     print(Z),
-    classify_not_peak(T, Y1, A, Z).
+    classify_not_peak(T, Y1, A, Z, B).
 
-classify_not_peak([machine(M, X, Z)|_], Y, [A|machine(M, Y, Z)], A).
+classify_not_peak([machine(M, X, N)|_], Y, A, A1, machine(M, Z, N)):-
+    append(A1, [machine(M, Y, N)], A),
+    Z is X - Y.
 
-classify_machine(X, Y, A):-
-    classify_not_peak(X, Y, A, []).
+classify_machine(X, Y, A, B):-
+    classify_not_peak(X, Y, A, [], B).
     
